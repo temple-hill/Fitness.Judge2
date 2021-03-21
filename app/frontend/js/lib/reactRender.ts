53c8a2a4d533666b42e12ca922a851068e57489d
teraoka <temple.hill@icloud.com>
2021-03-21 18:59:11 +0900

    eslintつかえる

--- app/frontend/js/lib/reactRender.ts
+++ app/frontend/js/lib/reactRender.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const reactRender = (renders: {[key: string]: JSX.Element}): void => {
-  const getElementData = (element: HTMLElement): object => {
+  const getElementData = (element: HTMLElement): Record<string, unknown> => {
     const dataset = element.dataset;
     let data = {};
     if (dataset['props']) {
